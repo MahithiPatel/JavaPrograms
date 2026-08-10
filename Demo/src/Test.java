de625231e8e6d94f4f455eaebd54c9e8563e5298
MAHITHI PATEL <MAHITHI PATEL@DESKTOP-HHVE6B3>
2026-08-10 21:16:02 +0530

    This is my second commit

--- Demo/src/Test.java
+++ Demo/src/Test.java
@@ -2,7 +2,9 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 10, b = 30;
+		int  c = a+b;
+		System.out.println("Addition: "+c);
 
 	}
 
