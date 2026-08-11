60924e0b0fcbc6743a9753a0955d736a23d342c5
MAHITHI PATEL <MAHITHI PATEL@DESKTOP-HHVE6B3>
2026-08-11 13:16:11 +0530

    display by continue stmt

--- Demo/src/Display.java
+++ Demo/src/Display.java
@@ -1,9 +1,11 @@
-
 public class Display {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        for(int i=1;i<=10;i++) {
+        	if(i==5) continue; // i%5==0;  5,10[doesn't print]
+        	System.out.println(i+ " ");
+        }
 	}
 
 }
