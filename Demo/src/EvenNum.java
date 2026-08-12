bec0677c6c6b7ff99ca765d29e51685a81416ac3
MahithiPatel <mahithipatelamudala@gmail.com>
2026-08-12 21:14:29 +0530

    Update EvenNum.java

--- Demo/src/EvenNum.java
+++ Demo/src/EvenNum.java
@@ -1,4 +1,10 @@
-
-public class EvenNum {
-
+public class EvenNum{
+	public static void main(String[] args) {
+		System.out.println("Even numbers: ");
+		for (int i = 1; i<=100;i++) {
+			if (i%2==0) {
+				System.out.println(" "+i);
+			}
+		}
+	}
 }
